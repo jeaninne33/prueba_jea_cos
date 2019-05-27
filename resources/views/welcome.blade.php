ee22a19aee4401d16ce933e122b1f3fcd375186b
Jeaninne Rivera <jeaninne.rivera@infotegra.com>
2019-05-27 16:18:53 -0500

    se crea el repo y se incluye plantilla

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends( 'layouts.layout' )
+@section('container')
+<div class="row">
+    <div class="col-xl-3 col-ml-6 col-mdl-4 col-sm-6 mt-5">
+        <div class="card">
+            <div class="pricing-list">
+                <div class="prc-head">
+                    <h4>Prueba Técnica Vecindad del chavo Jeaninne Rivera</h4>
+                </div>
+                <div class="prc-list">
+                    <ul>
+                      
+                    </ul>
+                    <a href="{{route('image-gallery.index')}}">Ver Galeria Vecindad del Chavo</a>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+</div>
+         
+ @endsection
